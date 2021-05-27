975116732a2b1dac61d5be24c2e87d47b6e23e3e
AbdElrazakElraz <>
2021-05-27 15:29:20 +0000

    js

---
+++ app.js
@@ -0,0 +1,9 @@
+alert("Wlc To the party")
+var s = prompt("Who Is Your Favorite Singer !")
+alert ("Attending the party requires you to be less than 75 KG")
+confirm("If you are more than 75 KG No entry !");
+var x = prompt("weight");
+//var y = prompt("height")
+//var z ="x + y";
+if (x > 75 ) { document.write ('<img src="https://img.memecdn.com/youre-fat_o_576964.jpg"')
+} else {document.write ('<img src="https://thumbs.dreamstime.com/b/welcome-to-party-vector-hand-drawn-illustration-phrases-welcome-to-party-lettering-idea-poster-postcard-117369564.jpg" ')}
