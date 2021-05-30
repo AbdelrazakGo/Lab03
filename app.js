332632bf835e24083f5d43b675fb4f90b8484f6f
AbdElrazakElraz <>
2021-05-30 13:06:21 +0000

    while and for

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
-alert("Wlc To the party")
-var s = prompt("Who Is Your Favorite Singer !")
-alert ("Attending the party requires you to be less than 75 KG")
-confirm("If you are more than 75 KG No entry !");
-var x = prompt("weight");
-//var y = prompt("height")
-//var z ="x + y";
-if (x > 75 ) { document.write ('<img src="https://img.memecdn.com/youre-fat_o_576964.jpg"')
-} else {document.write ('<img src="https://thumbs.dreamstime.com/b/welcome-to-party-vector-hand-drawn-illustration-phrases-welcome-to-party-lettering-idea-poster-postcard-117369564.jpg" ')}
+// alert("Wlc To the party")
+// var s = prompt("Who Is Your Favorite Singer !")
+// alert ("Attending the party requires you to be less than 75 KG")
+// confirm("If you are more than 75 KG No entry !");
+// var x = prompt("weight");
+// //var y = prompt("height")
+// //var z ="x + y";
+// if (x > 75 ) { document.write ('<img src="https://img.memecdn.com/youre-fat_o_576964.jpg"')
+// } else {document.write ('<img src="https://thumbs.dreamstime.com/b/welcome-to-party-vector-hand-drawn-illustration-phrases-welcome-to-party-lettering-idea-poster-postcard-117369564.jpg" ')}
+
+var today =['Monday', 'Tuesday', 'Wednesday',' Thursday', 'Friday' ,'Saturday', 'Sunday']
+alert ("Kindly Hit Enter")
+var userName = prompt ("userName")
+// var today = "sunday"
+alert ("welcom " + userName)
+today =prompt ("what is the day today")
+while (today != 'sunday'){ today = prompt("Rethink again , Hint it's sunday")}
+
+var star =['1','2','3','4','5']
+
+star = prompt("What star rating would you give, 1-5?")
+
+for ( i = 0 ; i < star.length ; i++ )
+if (star == 1) { document.write ('<img src="https://www.pngkit.com/png/detail/199-1996232_1-star-rating-transparent.png"')} 
+
+if (star == 2) { document.write ('<img src="https://pitchpivot.com/wp-content/uploads/2014/11/Two-star-rating.png"')} 
+
+if (star == 3) { document.write ('<img src="https://raw.githubusercontent.com/ami1906/react-star-rating-lite/develop/public/filled.png"')} 
+
+if (star == 4) { document.write ('<img src="https://icon-library.com/images/4-star-icon/4-star-icon-23.jpg"')} 
+
+if (star == 5) { document.write ('<img src="https://www.pngix.com/pngfile/middle/3-31473_5-star-accredited-school-5-gold-stars-png.png"')} 
+
+if (star > 5) alert ("Gratfull") 
+document.write ('<img src ="https://image.shutterstock.com/image-vector/love-you-much-260nw-645974524.jpg"')
